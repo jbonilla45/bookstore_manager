e99316c67f5ca05f4b11c04644fa4dfbab521007
jbonilla45 <jorge.bonilla12@misena.edu.co>
2021-10-13 20:13:12 -0500

    created book repository class

---
+++ src/main/java/com/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.bookstoremanager.repository;
+
+import com.bookstoremanager.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository <Book, Long> {
+}
